60f50d66cd1fcd2d8441611ab13af1ba29a82241
sadman_ichha <sadmanichha1@gmail.com>
2022-08-27 10:24:44 +0600

    added

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_crud_operation/views/add_new_course.dart';
 import 'package:flutter/material.dart';
 
@@ -11,10 +12,14 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   addNewCourse() {
     return showModalBottomSheet(
-      backgroundColor: Colors.transparent,
-      isScrollControlled: true,
-        context: context, builder: (context) => AddNewCourse());
+        backgroundColor: Colors.transparent,
+        isScrollControlled: true,
+        context: context,
+        builder: (context) => AddNewCourse());
   }
+
+  final Stream<QuerySnapshot> _coruseStream =
+      FirebaseFirestore.instance.collection("courses").snapshots();
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +35,39 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
+      body: StreamBuilder(
+          stream: _coruseStream,
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Center(child: Text('Something went wrong'));
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+            return ListView(
+              children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                Map<String, dynamic> data =
+                    document.data()! as Map<String, dynamic>;
+                return Container(
+                  height: 235.0,
+                  child: Column(
+                    // mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(height: 10.0),
+                      Expanded(child: Image.network(data['image'])),
+                      SizedBox(height: 8.0),
+                      Text(
+                        data['course_name'],
+                        style: TextStyle(fontSize: 25.0),
+                      ),
+                      Text(data['course_details']),
+                    ],
+                  ),
+                );
+              }).toList(),
+            );
+          }),
     );
   }
 }
